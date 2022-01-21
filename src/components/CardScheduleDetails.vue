030be10ca86b46b95222fc62cf465f84572fbd92
noor3030 <nnorkrka@gmail.com>
2022-01-21 23:03:01 +0300

    Add card

---
+++ src/components/CardScheduleDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    v-if="card != null"
+    :style="{ 'background-color': card.lesson.teacher.color }"
+    id="card-div"
+  >
+    <p v-if="card.lesson.teacher_id != null">
+      {{ card.lesson.teacher.name }}
+    </p>
+    <p>
+      {{ card.lesson.subject.name }}
+    </p>
+    <p v-if="card.lesson.room_id != null">
+      {{ card.lesson.room.name }}
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    card: {
+      type: Object,
+    },
+  },
+});
+</script>
+<style scoped>
+#card-div {
+  text-align: middle;
+  height: 16.6%;
+}
+</style>
--- src/views/ScheduleDetails.vue
+++ src/views/ScheduleDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container style="height: 100%; width: 100%">
-    <table height="80%">
+    <table height="100%" width="100%">
       <thead>
         <tr>
           <th></th>
@@ -12,7 +12,9 @@
       <tbody>
         <tr v-for="day in schedule.days" :key="day.id">
           <td>{{ day.name }}</td>
-          <td v-for="period in schedule.periods" :key="period.id"></td>
+          <td v-for="period in schedule.periods" :key="period.id">
+            <CardScheduleDetails :card="getCard(period.id, day.id)" />
+          </td>
         </tr>
       </tbody>
     </table>
@@ -22,11 +24,12 @@
 import Vue from "vue";
 import axios from "axios";
 import types from "@/CardTypes";
+import CardScheduleDetails from "@/components/CardScheduleDetails.vue";
 export default Vue.extend({
   data() {
     return {
       id: "",
-      Schedule: {} as types.Schedule,
+      schedule: {} as types.Schedule,
     };
   },
   created() {
@@ -34,9 +37,32 @@ export default Vue.extend({
     axios
       .get(`https://csuot.herokuapp.com/v1/schedule/stage/${this.id}`)
       .then((response) => {
-        this.Schedule = response.data;
-        console.log(this.Schedule);
+        this.schedule = response.data;
+
+        console.log(this.schedule.periods);
       });
   },
+  methods: {
+    getCard(period_id: string, day_id: string) {
+      for (let card of this.schedule.cards) {
+        if (card.period_id === period_id && card.day_id === day_id) return card;
+      }
+    },
+  },
+  components: { CardScheduleDetails },
 });
 </script>
+<style scoped>
+td {
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  text-align: middle;
+  vertical-align: middle;
+}
+tr {
+  display: table-row;
+  height: 1px;
+  vertical-align: inherit;
+  border-color: inherit;
+}
+</style>
